58f4273906c8e80fb05b3af0c6cb72dba47b847d
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-10 23:48:12 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,65 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+  
+  def save
+    # Check for duplicates
+    if @id
+      update
+    else
+      sql = <<-SQL
+        INSERT INTO students(name, grade) VALUES(?, ?)
+      SQL
+      DB[:conn].execute(sql, @name, @grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+  
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, @name, @grade, @id)
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS students")
+  end
+  
+  def self.create(name, grade)
+    self.new(name, grade).save
+  end
+  
+  def self.new_from_db(row)
+    self.new(row[0], row[1], row[2])
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+    self.new_from_db(DB[:conn].execute(sql, name)[0])
+  end
 
 
 end
